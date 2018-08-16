04ca6d63f6ab894bf89b9eb9c2160f2d10f703ba
junhobaik <junhobaik@gmail.com>
2018-08-16 18:39:37 +0900

    version 1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mojave Chrome Bug Fix (Chrome Extension)
+
+<a target="_blank" href="https://chrome.google.com/webstore/detail/nppenpbogpknnoghghdomklekifdhjkd">Download (Chrome Web Store)</a>
+
+
+Fix chrome checkbox and radiobutton invisible issue on macOS Mojave 10.14(beta)
+
+Change the scale(x1.0000001) to fix the bug,  
+and scale only applies to check boxes and radio buttons.
---
+++ watch.js
@@ -0,0 +1,22 @@
+const zoomLevel = 1.0000001;
+const style = {
+  zoom: zoomLevel
+};
+
+const changeElementsStyle = () => {
+  const elements = [
+    ...document.querySelectorAll("input[type='checkbox']"),
+    ...document.querySelectorAll("input[type='radio']")
+  ];
+  if (elements.length) {
+    for (let el of elements) {
+      Object.assign(el.style, style);
+    }
+  }
+};
+
+changeElementsStyle();
+
+window.onload = () => {
+  changeElementsStyle();
+};
