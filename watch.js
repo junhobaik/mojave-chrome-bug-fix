cfcbc336e9b768125b113c8f18d9ecdcbdce8812
junhobaik <junhobaik@gmail.com>
2018-08-17 19:34:56 +0900

    Modifying to work only in macOS

--- watch.js
+++ watch.js
@@ -1,22 +1,24 @@
-const zoomLevel = 1.0000001;
-const style = {
-  zoom: zoomLevel
-};
+if (navigator.appVersion.indexOf("Mac") !== -1) {
+  const zoomLevel = 1.0000001;
+  const style = {
+    zoom: zoomLevel
+  };
 
-const changeElementsStyle = () => {
-  const elements = [
-    ...document.querySelectorAll("input[type='checkbox']"),
-    ...document.querySelectorAll("input[type='radio']")
-  ];
-  if (elements.length) {
-    for (let el of elements) {
-      Object.assign(el.style, style);
+  const changeElementsStyle = () => {
+    const elements = [
+      ...document.querySelectorAll("input[type='checkbox']"),
+      ...document.querySelectorAll("input[type='radio']")
+    ];
+    if (elements.length) {
+      for (let el of elements) {
+        Object.assign(el.style, style);
+      }
     }
-  }
-};
+  };
 
-changeElementsStyle();
-
-window.onload = () => {
   changeElementsStyle();
-};
+
+  window.onload = () => {
+    changeElementsStyle();
+  };
+}
